79e573ea0d932c3b1fa807bf339b44dcf656bd25
momoko0529 <karta43215@gmail.com>
2019-12-24 19:50:57 +0800

    Initial Commit

---
+++ iosFinal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iosFinal
+//
+//  Created by User14 on 2019/12/24.
+//  Copyright © 2019 tflee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
