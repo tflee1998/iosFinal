35c495fc3ab1b23f21c0f90dc8d6dd23c3f96f36
momoko0529 <karta43215@gmail.com>
2019-12-24 22:59:59 +0800

    v1.0

--- iosFinal/ContentView.swift
+++ iosFinal/ContentView.swift
@@ -9,8 +9,34 @@
 import SwiftUI
 
 struct ContentView: View {
+
+func downloadTeamResult(){
+    print("hi")
+let urlStr="https://api.sportradar.us/nba-t3/zh/seasontd/2019/REG/rankings.json?api_key=ybweczu8fvfb249haydvuu3m"
+
+if let url = URL(string: urlStr) {
+
+     URLSession.shared.dataTask(with: url) { (data,response,error) in
+        let decoder=JSONDecoder()
+        if let data=data, let rankArr = try?decoder.decode(rankArr.self,from:data)
+        {
+            print(rankArr.league)
+            print("hell0")
+        }
+     }.resume()
+
+ }
+}
+
+
+    
     var body: some View {
-        Text("Hello World")
+        Button(action: {
+            self.downloadTeamResult()
+        }) {
+            Text("Hello World")
+        }
+        
     }
 }
 
